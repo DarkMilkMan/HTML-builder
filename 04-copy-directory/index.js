ad5d708c2f503324f6ce109d1139958cbea1f33d
Dmitry <darkmilkman@gmail.com>
2022-11-06 12:01:36 +0300

    feat: 04-copy-directory work

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
 
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'files');
+const folderCopy = path.join(__dirname, 'files-copy');
+
+async function folderCreate() {
+    await fs.promises.mkdir(folderCopy, {recursive: true});
+};
+
+async function performance() {
+    const itemFolderCopy = await fs.promises.readdir(folderCopy);
+    const itemFolder = await fs.promises.readdir(folder);
+    for (const file of itemFolderCopy) {
+        fs.promises.unlink(path.join(__dirname, 'files-copy', file));
+    };
+    if (itemFolder) {
+        for (const file of itemFolder) {
+            await fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+        }
+    };
+};
+
+folderCreate();
+performance();
